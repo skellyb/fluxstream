4f4737463ac1f18e41372f8de2d3560f1928c253
skellyb <skellyb@gmail.com>
2014-12-05 16:39:34 -0600

    Initial tests setup

---
+++ test/createAction.test.js
@@ -0,0 +1,16 @@
+var test = require('tape');
+
+test('Action Creator', function(t) {
+    t.plan(7);
+    t.skip('create action');
+    t.skip('listen returns an event payload');
+    t.skip('listenOnce returns an event payload');
+    t.skip('listenOnce unsubs self');
+    t.skip('validate function called before event is sent');
+    t.skip('validate function lets valid event through');
+    t.skip('errors capure validation error');
+});
+
+test.skip('Create Multiple Actions', function(t) {
+    
+});
