6aaa0770662f04544294e05315787426c6862f1a
skellyb <skellyb@gmail.com>
2015-01-12 12:22:05 -0600

    Test input handler and caches

--- test/createStore.test.js
+++ test/createStore.test.js
@@ -150,3 +150,120 @@ test('Bad store config', function(t) {
         t.equal(store.streams.testProp, undefined, 'Streaming prop should not be configured if undefined action used');
     })();
 });
+
+test('Store input stream and caching', function(t) {
+    t.plan(7);
+
+    // Test input handler
+    var fetchAction = createAction();
+    var receiveAction = createAction();
+    var store = createStore({
+        config: {
+            testProp: {
+                action: receiveAction,
+                inputAction: fetchAction,
+                inputHandler: function(payload) {
+                    receiveAction('All done.');
+                }
+            }
+        }
+    });
+
+    fetchAction();
+    store.streams.testProp.listen(function(payload) {
+        t.equal(payload, 'All done.', 'Store should handle input action.');
+    });
+
+    // Test cache
+    var data = {
+        obj1: {
+            id: 'obj1',
+            value: 'Into the cache.'
+        }, 
+        obj2: {
+            id: 'obj2',
+            value: 'Returned in a dash.'
+        }
+    };
+
+    fetchAction = createAction();
+    receiveAction = createAction();
+    store = createStore({
+        config: {
+            testProp: {
+                action: receiveAction,
+                init: { id: 'default', value: 'Nothing to see here.' },
+                inputAction: fetchAction,
+                inputHandler: function(payload) {
+                    receiveAction( data[payload.name] );
+                },
+                cacheKey: 'id',
+                inputKey: 'name'
+            }
+        }
+    });
+
+    var results = [];
+    fetchAction({ name: 'obj1' });
+    store.streams.testProp.listen(function(payload) {
+        results.push(payload.value);
+    });
+    fetchAction({ name: 'obj2' });
+    
+    // mutate data to mock fetched update
+    data = {
+        obj1: {
+            id: 'obj1',
+            value: 'Update in a flash.'
+        }
+    };
+
+    fetchAction({ name: 'obj1' });
+
+    t.equal(results[0], 'Into the cache.', 'Prop returns current value.');
+    t.equal(results[1], 'Nothing to see here.', 'After fetching different item, initial result is default (init) value.');
+    t.equal(results[2], 'Returned in a dash.', 'Then fetch completes with requested value.');
+    t.equal(results[3], 'Into the cache.', 'Fetching a cached value returns cache right away.');
+    t.equal(results[4], 'Update in a flash.', 'Finally, fetch sends updated value');
+
+
+    // Test cache limit
+    var data = {
+        obj1: {
+            id: 'obj1',
+            value: 'Into the cache.'
+        }, 
+        obj2: {
+            id: 'obj2',
+            value: 'Returned in a dash.'
+        },
+        obj3: {
+            id: 'obj3',
+            value: 'One too many.'
+        }
+    };
+
+    fetchAction = createAction();
+    receiveAction = createAction();
+    store = createStore({
+        config: {
+            testProp: {
+                action: receiveAction,
+                init: { id: 'default', value: 'Nothing to see here.' },
+                inputAction: fetchAction,
+                inputHandler: function(payload) {
+                    receiveAction( data[payload] );
+                },
+                cacheKey: 'id',
+                cacheLimit: 2
+            }
+        }
+    });
+
+    fetchAction('obj1');
+    fetchAction('obj2');
+    fetchAction('obj3');
+    var numCached = Object.keys(store.streams.testProp.cache).length
+    t.equal(numCached, 2, 'Cached items shouldn\'t exceed cacheLimit');
+
+});
