781caa5ca51c20f00db3a42bcf39f418300c3ade
skellyb <skellyb@gmail.com>
2015-04-17 16:55:48 -0500

    Streamline Actions a bit
    - Handler doesn't need to be built into the action. Keep the pipes dumb.
    - change listen method name to observe
    - Apply standard formatting

--- src/action.js
+++ src/action.js
@@ -1,27 +1,23 @@
-const Bacon = require('baconjs');
+const Bacon = require('baconjs')
 
 class Action {
-    constructor(handler) {
-        const handler = handler || Function.prototype;
-        const _dispatcher = new Bacon.Bus();
-        const _func = (payload) => {
-            handler(payload);
-            _dispatcher.push(payload)
-        };
+  constructor () {
+    const _dispatcher = new Bacon.Bus()
+    const _func = (payload) => _dispatcher.push(payload)
 
-        _func.stream = () => _dispatcher;
+    _func.stream = () => _dispatcher
 
-        _func.listen = (callback) => _dispatcher.onValue(callback);
+    _func.observe = (callback) => _dispatcher.onValue(callback)
 
-        _func.once = (callback) => {
-            return _dispatcher.onValue((payload) => {
-                callback(payload);
-                return Bacon.noMore;
-            });
-        };
-
-        return _func;
+    _func.once = (callback) => {
+      return _dispatcher.onValue((payload) => {
+        callback(payload)
+        return Bacon.noMore
+      })
     }
+
+    return _func
+  }
 }
 
-module.exports = Action;
+module.exports = Action
