b1349f92e9acce703652a899e6e5ea379b439483
skellyb <skellyb@gmail.com>
2015-04-18 18:27:57 -0400

    remove old references

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 /**
- * FluxStream: A lightweight Flux solution that controls data flow around your 
- * app using Bacon.js EventStreams. It pairs well with React.js, but doesn't 
+ * FluxStream: A lightweight Flux solution that controls data flow around your
+ * app using Bacon.js EventStreams. It pairs well with React.js, but doesn't
  * require it. Most of the smart thinking happened here: spoike/refluxjs. Having
  * each action control its own dispatcher was a brilliant way to streamline Flux.
  *
  * You don't have to use Bacon.js anywhere else in your app, but if you want to,
  * it's easy take these EventStreams and manipulate them at your leisure.
- * 
+ *
  * @module fluxstream
  */
-module.exports = {
-    createAction: require('./lib/createAction'),
-    createActions: require('./lib/createActions'),
-    createStore: require('./lib/store').createStore,
-    defineStore: require('./lib/store').defineStore
-};
+
+module.exports = require('./lib/core')
